781355bd03a19c469fc39a5e56a8ec4b9ee7c697
andrecontte-hub <andrecontte@gmail.com>
2025-11-08 22:13:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '🚀 Flask rodando com sucesso no Render e na Vercel!'
+
+if __name__ == '__main__':
+    # Render usa 0.0.0.0:10000+, Vercel ignora e injeta porta automaticamente
+    app.run(host='0.0.0.0', port=5000)
