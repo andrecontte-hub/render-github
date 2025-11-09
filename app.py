0a582a5cc24b393ae999c3df40893b7b91e78d57
andrecontte-hub <andrecontte@gmail.com>
2025-11-08 23:03:33 -0300

    Change route to render index.html with Flask

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
-def serve_player():
-    return send_from_directory('.', 'index.html')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(debug=True)
