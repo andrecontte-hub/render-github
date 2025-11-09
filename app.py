3350c3d3934509f27c8df1294695b3b9cc5943c8
andrecontte-hub <andrecontte@gmail.com>
2025-11-08 22:26:21 -0300

    Update index route to render HTML template

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '🚀 Flask rodando com sucesso no Render e na Vercel!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # Render usa 0.0.0.0:10000+, Vercel ignora e injeta porta automaticamente
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
