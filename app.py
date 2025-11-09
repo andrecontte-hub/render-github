d9606a20d003ee4bfcb3fb229e7cd969b90f9c13
andrecontte-hub <andrecontte@gmail.com>
2025-11-08 21:55:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>🚀 Flask rodando no Render/Vercel com sucesso!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
